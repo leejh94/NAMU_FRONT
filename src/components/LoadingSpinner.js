ff366a027cbd99cf72c8e4f044795a06d99faea0
leejh94 <kmlo121293@gmail.com>
2024-11-20 08:14:45 +0900

    [feat] : 로딩 스피너 추가

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,19 @@
+//src/components/LoadingSpinner.js
+import React from "react";
+import { useGlobalState } from "../context/GlobalStateContext";
+import loadingGif from "../assets/loading1.gif";
+import "./LoadingSpinner.scss";
+
+const LoadingSpinner = () => {
+  const { globalState } = useGlobalState();
+
+  if (!globalState.isLoading) return null; // 로딩 중이 아닐 때 렌더링하지 않음
+
+  return (
+    <div className="loading-overlay">
+      <img src={loadingGif} alt="Loading..." className="loading-spinner" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
