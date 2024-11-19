436d224c5fdc97dac3b8fc00d2c4b8d90eec8ce2
leejh94 <kmlo121293@gmail.com>
2024-11-20 08:15:22 +0900

    [feat] : 전역 변수 추가 및 StrictMode 모드 주석처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { GlobalStateProvider } from "./context/GlobalStateContext"; // 전역변수
+import LoadingSpinner from "./components/LoadingSpinner"; // 로딩스피너
 import Layout from "./layout/Layout";
 import Main from "./pages/Main";
 import Search from "./pages/Search/Search"; // Search 페이지 import
 import Stats from "./pages/Stats/Stats"; // Stats 페이지 import
 import Stats2 from "./pages/Stats/Stats2"; // Stats 페이지 import
 import Info from "./pages/Info"; // Search 페이지 import
+import Login from "./pages/Login/Login"; // Search 페이지 import
+import KakaoRedirect from "./pages/Login/KakaoRedirect"; // Search 페이지 import
 import "./App.scss"; // SCSS 파일 import
 import "primereact/resources/themes/saga-blue/theme.css"; // 테마 CSS
 import "primereact/resources/primereact.min.css"; // Core CSS
@@ -15,17 +19,21 @@ import "primeflex/primeflex.css"; // PrimeFlex 유틸리티 CSS (선택 사항)
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/Stats" element={<Stats />} />
-          <Route path="/Stats2" element={<Stats2 />} />
-          <Route path="/info" element={<Info />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <GlobalStateProvider>
+      <Router>
+        <Layout>
+          <LoadingSpinner />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/Stats" element={<Stats />} />
+            <Route path="/Stats2" element={<Stats2 />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/auth/login/callback" element={<KakaoRedirect />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </GlobalStateProvider>
   );
 }
 
---
+++ src/context/GlobalStateContext.js
@@ -0,0 +1,29 @@
+//src/context/GlobalContext.js
+import React, { createContext, useContext, useState } from "react";
+
+// Context 생성
+const GlobalStateContext = createContext();
+
+export const GlobalStateProvider = ({ children }) => {
+  const [globalState, setGlobalState] = useState({
+    isLoading: false, // 로딩 상태
+    isLoggedIn: false, // 로그인 여부
+  });
+
+  // 전역 상태 업데이트 함수
+  const updateGlobalState = (key, value) => {
+    setGlobalState((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  return (
+    <GlobalStateContext.Provider value={{ globalState, updateGlobalState }}>
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+// 커스텀 훅: Context 사용
+export const useGlobalState = () => useContext(GlobalStateContext);
