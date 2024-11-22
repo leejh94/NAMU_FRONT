f16db87b110d70eb22caaca31b02a490e5736a33
leejh94 <kmlo121293@gmail.com>
2024-11-22 18:13:59 +0900

    [feat] : 게시판 페이지 추가

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,15 @@
+// src/pages/Main.js
+import React from "react";
+import "./Board.scss";
+
+function Board() {
+  return (
+    <div className="board-page">
+      <div className="board-card">
+        <h4>회원게시판</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
