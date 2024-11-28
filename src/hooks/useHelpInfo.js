dc105fc4ae26d313abc6d961bb206731f81bc1fc
leejh94 <kmlo121293@gmail.com>
2024-11-29 02:41:08 +0900

    [feat] : 지원게시판 CRUD 로직 추가

--- src/hooks/useHelpInfo.js
+++ src/hooks/useHelpInfo.js
@@ -1,16 +1,39 @@
-import { useState, useEffect } from "react";
+// src/hooks/useHelpInfo.js"
+import { useState, useEffect, useRef } from "react";
 import {
   getInfoSiteList,
   siteAdd,
   siteDelete,
   siteNewOrderSave,
-  siteUpdate, // 업데이트 API 추가
+  siteUpdate,
+  getSupportBoardList,
+  supportBoardAdd,
+  supportBoardUpdate,
+  supporBoardDelete,
 } from "../service/helpInfoApi";
 import { useGlobalState } from "../context/GlobalStateContext";
 
 export function useHelpInfo() {
-  const [siteList, setSiteList] = useState([]);
   const { updateGlobalState } = useGlobalState(); // 글로벌 상태 업데이트 함수
+  const [siteList, setSiteList] = useState([]);
+  const [region, setRegion] = useState("전체");
+  const [supportList, setSupportList] = useState([]);
+
+  const isInitialRender = useRef(true); // 초기 렌더링 여부 저장
+
+  useEffect(() => {
+    fetchSiteList(); // 항상 초기 로드
+    fetchSupportList(region); // 초기 지원 게시글 로드
+  }, []); // 초기 렌더링 시에만 실행
+
+  useEffect(() => {
+    console.log(region);
+    if (isInitialRender.current) {
+      isInitialRender.current = false; // 초기 렌더링 이후 false로 설정
+    } else {
+      fetchSupportList(region); // 초기 렌더링 이후 region 변경 시 호출
+    }
+  }, [region]); // region이 변경될 때만 실행
 
   const fetchSiteList = async () => {
     updateGlobalState("isLoading", true);
@@ -95,16 +118,83 @@ export function useHelpInfo() {
     }
   };
 
-  useEffect(() => {
-    fetchSiteList();
-  }, []);
+  const fetchSupportList = async (region) => {
+    updateGlobalState("isLoading", true);
+    try {
+      const data = await getSupportBoardList(region);
+      setSupportList(data);
+    } catch (error) {
+      console.error("데이터 로드 오류:", error);
+    } finally {
+      updateGlobalState("isLoading", false);
+    }
+  };
+
+  const addSupportBoard = async (title, link, region) => {
+    updateGlobalState("isLoading", true);
+    try {
+      const data = await supportBoardAdd(title, link, region);
+      if (data.code === 200) {
+        // fetchSupportList(region);
+        setRegion(region);
+      }
+      return data;
+    } catch (error) {
+      console.error("지원 게시글 추가 실패:", error);
+      throw error;
+    } finally {
+      updateGlobalState("isLoading", false);
+    }
+  };
+
+  const deleteSupportBoard = async (supportId) => {
+    updateGlobalState("isLoading", true);
+    try {
+      const data = await supporBoardDelete(supportId);
+      if (data.code === 200) {
+        fetchSupportList(region);
+      }
+      return data;
+    } catch (error) {
+      console.error("지원 게시글 삭제 실패:", error);
+      throw error;
+    } finally {
+      updateGlobalState("isLoading", false);
+    }
+  };
+
+  const updatesupportBoard = async (supportId, title, link, region) => {
+    updateGlobalState("isLoading", true);
+
+    try {
+      const data = await supportBoardUpdate(supportId, title, link, region);
+      console.log(data);
+      if (data.code === 200) {
+        fetchSupportList(region);
+      }
+      return data;
+    } catch (error) {
+      console.error("사이트 수정 실패:", error);
+      throw error;
+    } finally {
+      updateGlobalState("isLoading", false);
+    }
+  };
 
   return {
+    region,
+    setRegion,
     siteList,
     setSiteList,
     addSite,
     deleteSite,
     saveNewOrder,
-    updateSite, // 수정 기능 추가
+    updateSite,
+    supportList,
+    setSupportList,
+    fetchSupportList,
+    addSupportBoard,
+    deleteSupportBoard,
+    updatesupportBoard,
   };
 }
--- src/service/helpInfoApi.js
+++ src/service/helpInfoApi.js
@@ -1,4 +1,4 @@
-// // src/service/helpInfoApi.js
+// src/service/helpInfoApi.js
 import { apiClient, authApiClient } from "./axiosConfig";
 
 // 유용한 사이트 목록 가져오기
@@ -77,6 +77,74 @@ export const siteUpdate = async (siteInfoId, img, link, title, description) => {
     return response.data; // 서버로부터의 응답 데이터 반환
   } catch (error) {
     console.error("사이트 수정 API 호출 오류:", error);
+    throw error;
+  }
+};
+
+// 서포트 게시판 추가
+export const supportBoardAdd = async (title, link, region) => {
+  try {
+    const response = await authApiClient.post("/helpInfo/addSupportBoard", {
+      title,
+      link,
+      region,
+    });
+    return response.data; // 서버 응답 데이터 반환
+  } catch (error) {
+    console.error("사이트 추가 API 호출 오류:", error);
+    throw error;
+  }
+};
+
+// 서포트 게시판 목록 가져오기
+export const getSupportBoardList = async (region) => {
+  try {
+    console.log("호출");
+    const response = await apiClient.get("/helpInfo/supportBoardList", {
+      params: { region }, // Query parameter로 region 전달
+    });
+    console.log(response.data.data);
+    return response.data.data; // 서버로부터의 응답 데이터 반환
+  } catch (error) {
+    console.error("사이트 목록 API 호출 오류:", error);
+    throw error; // 호출하는 곳에서 오류 처리할 수 있도록 오류 던지기
+  }
+};
+
+// 서프트 게시판 수정
+export const supportBoardUpdate = async (supportId, title, link, region) => {
+  try {
+    const payload = {
+      supportId,
+      title,
+      link,
+      region,
+    };
+
+    console.log(payload);
+    const response = await authApiClient.put(
+      "/helpInfo/updateSupportBoard",
+      payload
+    );
+    return response.data; // 서버로부터의 응답 데이터 반환
+  } catch (error) {
+    console.error("게시판 수정 API 호출 오류:", error);
+    throw error;
+  }
+};
+
+// 서포트 게시판 삭제
+export const supporBoardDelete = async (supportId) => {
+  try {
+    const response = await authApiClient.delete(
+      "/helpInfo/deleteSupportBoard",
+      {
+        params: { supportId },
+      }
+    );
+    return response.data; // 서버로부터의 응답 데이터 반환
+  } catch (error) {
+    console.error("서포트 게시판 삭제 API 호출 오류:", error);
     throw error;
   }
 };
